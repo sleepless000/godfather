b65044017e9d252ae979e21a9eed578f1205d737
Borislav Atanasov <b.v.atanasov@gmail.com>
2019-07-11 17:32:56 +0100

    Updates

---
+++ precache-manifest.eedcf55c6f0f4d7b82c04ac4b550126b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f54330b53bb4320fd9d795de7d932a",
+    "url": "/godfather/index.html"
+  },
+  {
+    "revision": "fc1bc4a8ec61671769dd",
+    "url": "/godfather/static/css/main.55241f86.chunk.css"
+  },
+  {
+    "revision": "a7bdd6acc98fe49b05e7",
+    "url": "/godfather/static/js/2.70bc83ec.chunk.js"
+  },
+  {
+    "revision": "fc1bc4a8ec61671769dd",
+    "url": "/godfather/static/js/main.9c06712e.chunk.js"
+  },
+  {
+    "revision": "3951009b4129f4bf45b4",
+    "url": "/godfather/static/js/runtime~main.f5326f04.js"
+  },
+  {
+    "revision": "94072296fb110d5aeada516f7658eddd",
+    "url": "/godfather/static/media/the-godfather.94072296.svg"
+  }
+]);
