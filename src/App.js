e702ffff7c9b666b0aab0ac02e0ba727a3b70315
Borislav Atanasov <b.v.atanasov@gmail.com>
2019-07-11 17:33:54 +0100

    adding gs pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import routes from './Router';
+import styled from 'styled-components';
+import logo from './_logo/the-godfather.svg';
+
+const Grid = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 30% 70%;
+`;
+
+const LeftGrid = styled.div`
+  padding-top: 5em;
+  display: grid;
+  grid-template-rows: 1fr 4fr;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 20px;
+  background: rgba(0, 0, 0, 0.4);
+`;
+const RightGrid = styled.div``;
+const List = styled.div`
+  padding-top: 5em;
+  list-style-type: none;
+`;
+
+const Logo = styled.img`
+  place-self: center;
+  height: 150px;
+`;
+
+const TabLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  font-size: 1.2em;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Grid>
+        <LeftGrid>
+          <Logo src={logo} />
+          <List>
+            <li>
+              <TabLink to="VitoCorleone">Vito Corleone</TabLink>
+            </li>
+            <li>
+              <TabLink to="/CarmeliaCorleone">Carmelia Corleone</TabLink>
+            </li>
+            <li>
+              <TabLink to="/CarloRizzi">Carlo Rizzi</TabLink>
+            </li>
+            <li>
+              <TabLink to="/LuciMancini">Luci Mancini</TabLink>
+            </li>
+          </List>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.sidebar}
+            />
+          ))}
+        </LeftGrid>
+
+        <RightGrid>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+            />
+          ))}
+        </RightGrid>
+      </Grid>
+    </Router>
   );
 }
 
